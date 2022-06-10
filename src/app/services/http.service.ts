f466468b2ed1be7f019096eb04fef7037816f111
Muhammad Nabeel <muhammad.nabeel@cubixlabs.com>
2022-06-10 21:00:41 +0500

    http services

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { forkJoin, map } from 'rxjs';
 import { Observable } from 'rxjs/internal/Observable';
 import { environment as env } from '../../environments/environment.prod';
 import { APIResponse, Game } from '../models';
@@ -8,6 +9,7 @@ import { APIResponse, Game } from '../models';
   providedIn: 'root',
 })
 export class HttpService {
+  [x: string]: any;
   constructor(private http: HttpClient) {}
   getGameList(
     ordering: string,
@@ -17,8 +19,28 @@ export class HttpService {
     if (search) {
       params = new HttpParams().set('ordering', ordering).set('search', search);
     }
-    return this.http.get<APIResponse<Game>>(`${env.BASE_URL}`, {
+    return this.http.get<APIResponse<Game>>(`${env.BASE_URL}/games${env.SECRET_KEY}`, {
       params: params,
     });
   }
+  getGameDetails(id: string): Observable<Game> {
+    const gameInfoRequest = this.http.get(`${env.BASE_URL}/games/${id}${env.SECRET_KEY}`);
+    const gameTrailerRequest = this.http.get(
+      `${env.BASE_URL}/games/${id}${env.SECRET_KEY}`
+    );
+    const gameScreenShortsRequests = this.http.get(
+      `${env.BASE_URL}/games/${id}${env.SECRET_KEY}`
+    );  return forkJoin({
+      gameInfoRequest,
+      gameScreenShortsRequests,
+      gameTrailerRequest
+    }).pipe(map((resp:any)=>{
+      return{
+        ...resp['gameInfoRequest'],
+        screenshorts:resp['gameScreenShortsRequests']?.results,
+        trailers:resp['gameTrailerRequest']?.results
+      }
+    }))
+  }
+
 }
