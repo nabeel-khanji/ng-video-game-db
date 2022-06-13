1ffc2da26b1181c3f9b2b05e100f635d4a1c9593
Muhammad Nabeel <muhammad.nabeel@cubixlabs.com>
2022-06-13 21:05:25 +0500

    details

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Game } from 'src/app/models';
@@ -9,7 +9,7 @@ import { HttpService } from 'src/app/services/http.service';
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss'],
 })
-export class DetailsComponent implements OnInit {
+export class DetailsComponent implements OnInit, OnDestroy {
   gameRating = 0;
   gameID!: string;
   game!: Game;
@@ -30,6 +30,7 @@ export class DetailsComponent implements OnInit {
       return '#ef4655';
     }
   }
+
   ngOnInit(): void {
     this.routeSub = this.activatedRoute.params.subscribe((params: Params) => {
       this.gameID = params['id'];
@@ -45,5 +46,13 @@ export class DetailsComponent implements OnInit {
           this.gameRating = this.game.metacritic;
         }, 1000);
       });
+  }
+  ngOnDestroy():void{
+    if (this.gameSub){
+      this.gameSub.unsubscribe();
+    }
+    if (this.routeSub){
+      this.routeSub.unsubscribe();
+    }
   }
 }
