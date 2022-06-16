61c74fad2643e298d69fd02348ae6a9950ff8b20
Muhammad Nabeel <muhammad.nabeel@cubixlabs.com>
2022-06-16 18:34:16 +0500

    home chages

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,20 +10,40 @@ import { HttpService } from 'src/app/services/http.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit, OnDestroy {
+  collection = { count: 60, data: [{}] };
+  config = {
+    itemsPerPage: 5,
+    currentPage: 1,
+    totalItems: this.collection.count,
+  };
   public sort!: string;
   public games!: Array<Game>;
   public count!: number;
   public next!: string;
   public previous!: string;
-  
+  public page: number=1;
+
   public gameSub!: Subscription;
   public routeSub!: Subscription;
-
+  public labels: any = {
+    previousLabel: '<--',
+    nextLabel: '-->',
+    screenReaderPaginationLabel: 'Pagination',
+    screenReaderPageLabel: 'page',
+    screenReaderCurrentLabel: this.page,
+  };
   constructor(
     private httpService: HttpService,
     private router: Router,
     private activatedRoute: ActivatedRoute
-  ) {}
+  ) {
+    for (var i = 0; i < this.collection.count; i++) {
+      this.collection.data.push({
+        id: i + 1,
+        value: 'items number ' + (i + 1),
+      });
+    }
+  }
 
   ngOnInit(): void {
     this.routeSub = this.activatedRoute.params.subscribe((params: Params) => {
@@ -32,35 +52,35 @@ export class HomeComponent implements OnInit, OnDestroy {
       } else {
         this.searchGames('metacrit');
       }
-      
     });
   }
-  searchGames( sort: string, search?: string) {
+  searchGames(sort: string, search?: string) {
     this.gameSub = this.httpService
-      .getGameList(sort, search)
+      .getGameList(sort, 1, search)
       .subscribe((gameList: APIResponse<Game>) => {
         this.games = gameList.results;
-        this.count= gameList.count;
-        this.next= gameList.next;
-        this.previous= gameList.previous;
-        
+        this.count = gameList.count;
+        this.next = gameList.next;
+        this.previous = gameList.previous;
+        this.page = gameList.page;
+
         console.log(gameList);
       });
   }
-  gamePagination(link:string){
-    console.log(link);
+  gamePagination(page: number) {
     
+    console.log(this.page);
+
     this.gameSub = this.httpService
-    .getGameList(link)
-    .subscribe((gameList: APIResponse<Game>) => {
-      this.games = gameList.results;
-      this.count= gameList.count;
-      this.next= gameList.next;
-      this.previous= gameList.previous;
-      
-      console.log(gameList);
-      
-    });
+      .getGameList('', this.page)
+      .subscribe((gameList: APIResponse<Game>) => {
+        this.games = gameList.results;
+        this.count = gameList.count;
+        this.next = gameList.next;
+        this.previous = gameList.previous;
+
+        console.log(gameList);
+      });
   }
   openGameDetailsMethod(id: string): void {
     this.router.navigate(['details', id]);
@@ -72,5 +92,27 @@ export class HomeComponent implements OnInit, OnDestroy {
     if (this.routeSub) {
       this.routeSub.unsubscribe();
     }
+  }
+  onPageChange(event: number) {
+    var page= 0;
+    if (this.page == null) {
+      this.page = 1;
+    }
+    if (page < this.page) {
+      this.page++;
+      page++;
+    }
+    if (page > this.page) {
+      this.page--;
+      page--;
+    }
+    console.log(this.page);
+    console.log(page);
+    
+    
+    console.log(event);
+    this.config.currentPage = event;
+    this.router.navigate(['page', event]);
+    this.gamePagination(this.page);
   }
 }
